0325a72a0d539acc850c525bfbe47bac0fb3f23d
Abhishek <120463498+abhi0920@users.noreply.github.com>
2023-05-26 10:51:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ app=Flask(__name__)
 @app.route('/')
 def hello():
 	return "Hello deployment of a web application"
-if __name__='main__':
+if __name__= '__main__':
 app.run(host='127.0.0.1', port=8080, debug=True)
